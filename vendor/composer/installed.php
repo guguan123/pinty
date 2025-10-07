5746e06b7595855da407c81d0f735953a3e5c178
GuGuan123 <admin@guguan.us.kg>
2025-10-08 00:44:14 +0800

    新文件：   vendor/

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'guguan123/pinty',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3097b6822e5b4e00155a9da2aa4c81fd0d9b7ad0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'guguan123/pinty' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3097b6822e5b4e00155a9da2aa4c81fd0d9b7ad0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
